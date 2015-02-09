f109ee8699621fd2a7dc85e36aff854404b34034
hongkunyoo <hongkunyoo@gmail.com>
2015-02-09 20:37:48 +0900

    d

--- js/controller/index.js
+++ js/controller/index.js
@@ -14,19 +14,5 @@ itemApp.controller('indexController', function($rootScope, $location, $rootScope
 		itService.azureService.switchServer(itService.azureService.SERVER.TEST);
 	};
 	
-	console.log('in index controller');
-	window.fbAsyncInit = function() {
-		FB.init({
-			appId : '734298746647918',
-			cookie : true, // enable cookies to allow the server to access
-			// the session
-			xfbml : true, // parse social plugins on this page
-			version : 'v2.1' // use version 2.1
-		});
-		FB.getLoginStatus(function(response) {
-			// statusChangeCallback(response);
-			console.log(JSON.stringify(response));
-		});
-	};
 	
 });
