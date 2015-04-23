b85670e01066e5554caba18e8a68c0b5b87f4707
hongkunyoo <hongkunyoo@gmail.com>
2015-04-23 22:24:38 +0900

    aa

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
-var itemApp = angular.module('itemApp', ['ngRoute', 'ngAnimate', 'infinite-scroll', 'ngStorage', 'azureBlobUpload', 'ui.router']); 
+var itemApp = angular.module('itemApp', ['ngRoute', 'ngAnimate', 'infinite-scroll', 'ngStorage', 'azureBlobUpload', 'ui.router', 'ngOnload']);
 
 itemApp.config(function($stateProvider, $urlRouterProvider) {
 	// $routeProvider
 	// .when('/', {
-		// templateUrl : 'partials/login.html'
+	// templateUrl : 'partials/login.html'
 	// }).when('/signup', {
-		// templateUrl : 'partials/signup.html'
+	// templateUrl : 'partials/signup.html'
 	// }).when('/signup/:inviteKey', {
-		// templateUrl : 'partials/signup.html'
+	// templateUrl : 'partials/signup.html'
 	// }).when('/invite', {
-		// templateUrl : 'partials/create_invite_key.html'
+	// templateUrl : 'partials/create_invite_key.html'
 	// }).when('/list', {
-		// templateUrl : 'partials/list.html'
+	// templateUrl : 'partials/list.html'
 	// }).when('/detail/:item_id', {
-		// templateUrl : 'partials/detail.html'
+	// templateUrl : 'partials/detail.html'
 	// }).when('/user_profile/:userId', {
-		// templateUrl : 'partials/user_profile.html'
+	// templateUrl : 'partials/user_profile.html'
 	// }).when('/test', {
-		// templateUrl : 'partials/test.html'
+	// templateUrl : 'partials/test.html'
 	// })
 	// .otherwise({
-		// redirectTo : '/login'
+	// redirectTo : '/login'
 	// });
-	
-	$urlRouterProvider
-        .otherwise('/list');
-	
+
+	$urlRouterProvider.otherwise('/list');
+
 	$stateProvider
 		// .state('login', {
             // url: '/login',
@@ -47,9 +46,10 @@ itemApp.config(function($stateProvider, $urlRouterProvider) {
             url: '/list',
             templateUrl: 'partials/list.html'
         })
-        .state('test', {
-            url: '/test',
-            templateUrl: 'partials/test.html'
+
+        .state('detail', {
+            url: '/detail',
+            templateUrl: 'partials/detail.html'
         })
         // .state('list.home', {
             // url: '/home',
--- js/service/object_pref_helper.js
+++ js/service/object_pref_helper.js
@@ -1,12 +1,14 @@
 itemApp.factory("prefHelper", function($rootScope, $localStorage) {
-	
+
 	return {
-		put: function(obj) {
+		put : function(obj) {
 			$localStorage.$default(obj);
-		}, get: function(key) {
+		},
+		get : function(key) {
 			return $localStorage[key];
-		}, remove: function(key) {
-			$localStorage.$reset(key);	
+		},
+		remove : function(key) {
+			$localStorage.$reset(key);
 		}
 	};
 });
